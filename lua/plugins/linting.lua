b77b601ee326555df1ad0f32aeebbbddf4c2b130
Chandan Chauhan <bellicose.agnostic@gmail.com>
2023-11-02 23:44:50 +0000

    Removed conflicting J keymap

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -14,14 +14,16 @@ return {
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+    
+    -- Linting before saving file requires linter to support it, pylint doesn't support it, eslint does
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()
       end,
     })
 
+    -- Linting before saving file requires linter to support it, pylint doesn't support it, eslint does
     vim.keymap.set("n", "<leader>l", function()
       lint.try_lint()
     end, { desc = "Trigger linting for current file" })
