e7fd6d22b0c2e0f1a807998a99d4954cbce9567c
Chandan Chauhan <bellicose.agnostic@gmail.com>
2024-01-24 21:00:48 -0800

    Updated section separator in lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -53,6 +53,7 @@ return {
     lualine.setup({
       options = {
         theme = my_lualine_theme,
+        component_separators = {left = '', right = '|'}
       },
       sections = {
         lualine_x = {
